7f7f278d2a6bdfb5cb59d1bf21175d316b01b599
destijl <gcastle@google.com>
2017-11-19 23:13:28 -0800

    Fix Dockerfile and init.sh

--- metasploit/scripts/init.sh
+++ metasploit/scripts/init.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-source /usr/local/rvm/scripts/rvm
 /etc/init.d/postgresql start
-/opt/msf/msfupdate --git-branch master
-/bin/bash -c "cd /tmp/data && bash"
+
+while true
+do
+  echo Entering infite sleep...
+  sleep 500000
+done
